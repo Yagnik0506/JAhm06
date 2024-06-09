1f1b71bf2ccd0bbeb2790888edbdc87cff666122
Yagnik0506 <kachayagnik136@gmail.com>
2024-06-09 14:17:35 +0530

    Add files via upload

---
+++ TNS/Project/Student.java
@@ -0,0 +1,9 @@
+package firstproject;
+
+public class Student {
+	public static void main(String arg[]) {
+	Emp em = new Emp();
+	em.setName("YAGNIK");
+	System.out.println(em.getName());
+	}
+}
---
+++ TNS/functionalinterface/Main.java
@@ -0,0 +1,12 @@
+package functionalinterface;
+
+public class Main {
+
+	public void sayhello() {
+		System.out.println("YAGNIK KACHA");
+	}
+	public static void main(String[] arg) {
+		Main m = new Main();
+		m.sayhello();
+	}
+}
